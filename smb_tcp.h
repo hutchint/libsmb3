f5114d56f047673830e8270ad4388d061d63b97d
Terrance Hutchinson <terrance.hutchinson@gmail.com>
2012-10-23 22:51:26 -0600

    more work on smb_tcp

--- smb_tcp.h
+++ smb_tcp.h
@@ -13,6 +13,10 @@
 #include <unistd.h>
 #include <sys/socket.h>
 
+/* Purpose: Base class that manages some of the TCP networking
+ *  Can be inherited or derived from
+ *
+ */
 class SMB_TCP
 {
 public:
@@ -22,12 +26,24 @@ public:
     char ip_address[3];
     int port;
     char mac_address[3];
+    char protocol;
+    char type; // Will be set to SOCK_STREAM in constructor
+    
+    // Methods
+    int connect(char *ip, int port, char type, char protocol);
+    int disconnect(char *ip, int port, char type, char protocol);
+    int listen();
+    int send();
+    int recv(int socket, void *r_buf, size_t length, int flags,
+             struct *sockaddr *restrict address, socklen_t *restrict address_len));
+    int 
+    
     
 private:
     bool isIPv4;
     bool isIPv6;
     bool is_network_up(int port);
+    
 
 
-
-}SMB_TCP, PSMB_TCP;
+};
